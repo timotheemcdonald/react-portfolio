7a30f18416ff9b662d1dce6d78dcc12191debc8e
timotheemcdonald <timotheemcdonald@gmail.com>
2020-11-16 23:12:02 -0500

    finished contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,9 @@ import styled from 'styled-components'
 
 const CenterDiv = styled.div`
 display:flex;
-justify-content:space-around;
 align-items:center;
+flex-direction:column;
+justify-content:space-around;
 border-bottom: .35rem solid #ced4da;
 background-color:#f8f9fa;
 padding:2%;
@@ -19,12 +20,32 @@ justify-content:center;
 margin-top: 2.50%;
 margin-bottom: 0.50%;
 `
+const TextSpan = styled.span`
+padding:1%;
+width:100%;
+margin-top:1%;
+`
+const LinkStyle = styled.a`
+color:black;
+background-color:white;
+border:2px solid black;
+padding: .25rem 1rem;
+text-decoration:none;
+margin-top:1rem;
+`
 
 function Contact(){
     return(
         <div>
             <CenterTitle><h2>Contact</h2></CenterTitle>
-            <CenterDiv><h2>Contact</h2></CenterDiv>
+            <CenterDiv>
+                <TextSpan>Want to get in touch? Feel free to send me a letter:</TextSpan>
+                <TextSpan><LinkStyle href="mailto:timotheemcdonald@gmail.com">timotheemcdonald@gmail.com</LinkStyle></TextSpan>
+                <TextSpan>We can also connect:</TextSpan>
+                <TextSpan><LinkStyle href="https://www.linkedin.com/in/tim-d-mcdonald/" target="_blank">LinkedIn</LinkStyle></TextSpan>
+                <TextSpan><LinkStyle href="https://github.com/timotheemcdonald" target="_blank">GitHub</LinkStyle></TextSpan>
+                <TextSpan><LinkStyle href="https://twitter.com/timotheemc" target="_blank">Twitter</LinkStyle></TextSpan>
+                </CenterDiv>
         </div>
     )
 }
